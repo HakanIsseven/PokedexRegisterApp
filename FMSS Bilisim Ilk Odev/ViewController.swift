2c2bb25ccb05d81ade645b47bd017c197f2f6e27
Hakanisseven <hakan.isseven@gmail.com>
2022-09-08 11:54:43 +0300

    Initial Commit

---
+++ FMSS Bilisim Ilk Odev/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FMSS Bilisim Ilk Odev
+//
+//  Created by Hakan İşseven on 8.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
